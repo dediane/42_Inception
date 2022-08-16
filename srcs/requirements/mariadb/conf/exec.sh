f9f9b1d464f7671a5b754c86825267a792be7471
dediane <ddecourt@student.42.fr>
2022-08-16 19:50:26 +0200

    up

--- srcs/requirements/mariadb/conf/exec.sh
+++ srcs/requirements/mariadb/conf/exec.sh
@@ -6,3 +6,7 @@ mysql -e "DELETE FROM mysql.user WHERE User='';";
 mysql -e "CREATE USER IF NOT EXISTS'$MYSQL_USER'@'localhost' identified by '$MYSQL_PASSWORD';";
 mysql -e "GRANT ALL PRIVILEGES ON *.* TO '$MYSQL_USER'@'localhost' IDENTIFIED BY '$MYSQL_PASSWORD';";
 mysql -e "FLUSH PRIVILEGES;"
+
+#killall mysqld
+
+#mysqld
