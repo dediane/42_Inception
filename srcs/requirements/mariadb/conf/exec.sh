dca370781bc4392196ff81a52f469fa41483efd0
dediane <ddecourt@student.42.fr>
2022-08-24 05:35:01 +0200

    up

--- srcs/requirements/mariadb/conf/exec.sh
+++ srcs/requirements/mariadb/conf/exec.sh
@@ -1,12 +1,16 @@
 #!/bin/bash
+#killall mysqld
+rm -rf /var/lib/mysql/*
+
+mysql_install_db
 
 service mysql start
 
 mysql -e "DELETE FROM mysql.user WHERE User='';";
-mysql -e "CREATE USER IF NOT EXISTS'$MYSQL_USER'@'localhost' identified by '$MYSQL_PASSWORD';";
-mysql -e "GRANT ALL PRIVILEGES ON *.* TO '$MYSQL_USER'@'localhost' IDENTIFIED BY '$MYSQL_PASSWORD';";
+mysql -e "CREATE USER IF NOT EXISTS'$MYSQL_USER'@'%' IDENTIFIED BY '$MYSQL_PASSWORD';";
+mysql -e "GRANT ALL PRIVILEGES ON wordpress.* TO '$MYSQL_USER'@'%'";
+mysql -e "ALTER USER 'root'@'localhost' IDENTIFIED BY '$MYSQL_ROOT_PASSWORD';";
 mysql -e "FLUSH PRIVILEGES;"
 
-killall mysqld
-
-mysqld
+#killall mysqld
+mysqld -u root -p
--- srcs/requirements/wordpress/configure.sh
+++ srcs/requirements/wordpress/configure.sh
@@ -1,22 +1,22 @@
 #!/bin/bash
 
 #Create folder for pid php file
-ls
 mkdir /run/php/
 cd /var/www/html/
 
-if [ ! -f /var/www/html/wordpress/wp-config.php ]; then 
+if [ ! -f /var/www/html/wp-config.php ]; then 
     wget https://raw.githubusercontent.com/wp-cli/builds/gh-pages/phar/wp-cli.phar
     chmod +x wp-cli.phar
     mv wp-cli.phar /usr/local/bin/wp
     wp core download --allow-root
 
-    cp  /usr/share/wordpress/wp-config.php ./wordpress
-    sed -i "s/MYSQL_USER/$MYSQL_USER/g" ./wp-config.php && \
-    sed -i "s/MYSQL_PASSWORD/$MYSQL_PASSWORD/g" ./wp-config.php && \
-    sed -i "s/MYSQL_DATABASE/$MYSQL_DATABASE/g" ./wp-config.php && \
-    sed -i "s/WP_DBHOST/$WP_DBHOST/g" ./wp-config.php
-
+    cp  /usr/share/wordpress/wp-config.php /var/www/html/wp-config.php
+    sed -i "s/MYSQL_USER/$MYSQL_USER/g" /var/www/html/wp-config.php && \
+    sed -i "s/MYSQL_PASSWORD/$MYSQL_PASSWORD/g" /var/www/html/wp-config.php && \
+    sed -i "s/MYSQL_DATABASE/$MYSQL_DATABASE/g" /var/www/html/wp-config.php && \
+    sed -i "s/WP_DBHOST/$WP_DBHOST/g" /var/www/html/wp-config.php
+    echo "CAT WP:" 
+    cat /var/www/html/wp-config.php
     wp core install --allow-root --url="$WP_URL" --title="$WP_TITLE" --admin_user="$WP_ADMIN" --admin_password="$WP_ADMIN_PWD" --admin_email="$WP_ADMIN_EMAIL" --skip-email
 
     echo "Wordpress installed & configured with Mariadb"
