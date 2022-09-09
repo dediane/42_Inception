769b1db97b1f5926a11d2b02420359cc4e9271e7
dediane <ddecourt@student.42.fr>
2022-09-09 14:04:20 +0200

    up

--- srcs/requirements/mariadb/conf/exec.sh
+++ srcs/requirements/mariadb/conf/exec.sh
@@ -1,19 +1,18 @@
 #!/bin/bash
 killall mysqld
 
-
-#mysql_install_db
+killall mysqld
+rm -rf /var/lib/mysql/*
+mysql_install_db
 
 service mysql start
-sleep 5
-#mysql -e "FLUSH PRIVILEGES;"
+
 mysql -e "DELETE FROM mysql.user WHERE User='';";
-mysql -e "ALTER USER 'root'@'localhost' IDENTIFIED BY '$MYSQL_ROOT_PASSWORD';";
 
 mysql -e "CREATE DATABASE wordpress;";
 mysql -e "CREATE USER IF NOT EXISTS '$MYSQL_USER'@'%' IDENTIFIED BY '$MYSQL_PASSWORD';";
 mysql -e "GRANT ALL PRIVILEGES ON wordpress.* TO '$MYSQL_USER'@'%' WITH GRANT OPTION;";
-mysql -e "FLUSH PRIVILEGES;"
+mysql -e "ALTER USER 'root'@'localhost' IDENTIFIED BY '$MYSQL_ROOT_PASSWORD';FLUSH PRIVILEGES;";
 
 
 killall mysqld
--- srcs/requirements/wordpress/configure.sh
+++ srcs/requirements/wordpress/configure.sh
@@ -15,8 +15,7 @@ if [ ! -f /var/www/html/wp-config.php ]; then
     sed -i "s/MYSQL_PASSWORD/$MYSQL_PASSWORD/g" /var/www/html/wp-config.php && \
     sed -i "s/MYSQL_DATABASE/$MYSQL_DATABASE/g" /var/www/html/wp-config.php && \
     sed -i "s/WP_DBHOST/$WP_DBHOST/g" /var/www/html/wp-config.php
-    echo "CAT WP:" 
-    cat /var/www/html/wp-config.php
+
     wp core install --allow-root --url="$WP_URL" --title="$WP_TITLE" --admin_user="$WP_ADMIN" --admin_password="$WP_ADMIN_PWD" --admin_email="$WP_ADMIN_EMAIL" --skip-email
 
     echo "Wordpress installed & configured with Mariadb"
